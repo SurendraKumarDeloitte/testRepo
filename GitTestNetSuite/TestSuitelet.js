8818eeaa6608404c1348dd87ee017e9e37f96ad9
SurendraKumarDeloitte <38937081+SurendraKumarDeloitte@users.noreply.github.com>
2018-06-05 17:17:25 +0530

    Update TestSuitelet.js

--- GitTestNetSuite/TestSuitelet.js
+++ GitTestNetSuite/TestSuitelet.js
@@ -1,7 +1,7 @@
 /**
  * @NApiVersion 2.x
  * @NScriptType Suitelet
- * @NModuleScope SameAccount test change again change third change forth change
+ * @NModuleScope SameAccount test change again change third change forth change fifth change
  */
 define(['N/record', 'N/search'],
 /**
